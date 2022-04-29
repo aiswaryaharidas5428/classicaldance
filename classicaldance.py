7848c301d24d3967ad0a8128a3522f48a5da296a
aiswaryaharidas5428 <69617828+aiswaryaharidas5428@users.noreply.github.com>
2022-04-29 16:24:18 +0530

    THIRD COMMIT.

--- classicaldance.py
+++ classicaldance.py
@@ -1,4 +1,9 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from flask import Flask, render_template, request, session
+from tensorflow.python.keras.models import load_model
 
 from dbconnection import Db
 
@@ -6,16 +11,30 @@ from dbconnection import Db
 app = Flask(__name__)
 app.secret_key = "hi"
 
-static_path="C:\\Users\\M G HARIDAS\\PycharmProjects\\classicaldance\\static\\"
+static_path="C:\\Final\\classicaldance\\classicaldance\\static\\"
+
 @app.route('/hi')
 def hello_world():
     return 'Hello World!'
 
 
 
+
+@app.route('/')
+def launching():
+    return render_template('launching_index.html')
+
+@app.route('/user_ind')
+def user_ind():
+    return render_template('user/usrhome.html')
+
+
+
+
 @app.route('/login')
 def login():
     return render_template('index.html')
+
 
 @app.route('/login_post',methods=['post'])
 def login_post():
@@ -30,7 +49,7 @@ def login_post():
         if res["type"]=="admin":
             return '''<script>alert('successs');window.location='/adminhome'</script>'''
         elif res["type"]=="user":
-            return '''<script>alert('successs');window.location='/usrhome'</script>'''
+            return '''<script>alert('successs');window.location='/user_ind'</script>'''
         else:
             return '''<script>alert('invalid');window.location='/login'</script>'''
     else:
@@ -172,11 +191,29 @@ def adminhome():
 
 
 
+@app.route('/admin_change_password')
+def admin_change_password():
+    return render_template('admin/user_change_password.html')
 
+@app.route('/admin_change_password_post',methods=['post'])
+def admin_change_password_post():
+    cu_pass=request.form['textfield']
+    nw_pass=request.form['textfield2']
+    re_type=request.form['textfield3']
+    db = Db()
+    qry = "select * from login where password='"+cu_pass+"'"
+    res=db.selectOne(qry)
+    if(nw_pass==re_type):
+        qry1="update login set password='"+nw_pass+"' where logid='"+str(session['lid'])+"'"
+        res2=db.update(qry1)
+        return '''<script>alert('password changed');window.location='/login'</script>'''
+    else:
+        return '''<script>alert('password not changed');window.location='/change_password'</script>'''
 
 @app.route('/change_password')
 def change_password():
     return render_template('user/passwdreset.html')
+
 @app.route('/change_password_post',methods=['post'])
 def change_password_post():
     cu_pass=request.form['textfield']
@@ -219,9 +256,12 @@ def user_reg_post():
 
     res=db.insert(qry)
     qry2="insert into user(ulogid,uname,udob,uphone,uimage,uemail)values('"+str(res)+"','"+fname+"','"+dob+"','"+phone+"','"+path+"','"+emailid+"')"
-    print(qry2)
+    # print(qry2)
     res2=db.insert(qry2)
-    return render_template('login.html',data=res2)
+
+    return render_template('index.html',data=res2)
+
+
 
 
 
@@ -237,5 +277,182 @@ def view_profile():
 def usrhome():
     return render_template('user/usrhome.html')
 
+@app.route('/upload')
+def upload():
+    return render_template('user/upload.html')
+
+
+
+@app.route("/uploadpostnew",methods=['post'])
+def a():
+    human_string=""
+    import tensorflow as tf
+    import sys
+    import os
+    import cv2
+    files = request.files["fileField"]
+    files.save("C:\\Final\\classicaldance\\classicaldance\\static\\2.jpg")
+    # Disable tensorflow compilation warnings
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    import tensorflow as tf
+    # image_path = sys.argv[1]
+    # image_path="C:\\Users\\ELCOT-Lenovo\\Documents\\images\\sign_dataset\\test\\A\\color_0_0016"
+    # Read the image_data
+    image_data = tf.gfile.FastGFile(r"C:\\Final\\classicaldance\\classicaldance\\static\\2.jpg",'rb').read()
+    # Loads label file, strips off carriage return
+    label_lines = [line.rstrip() for line
+                   in tf.gfile.GFile("C:\\Final\\classicaldance\\classicaldance\\logs\\output_labels.txt")]
+
+    with tf.gfile.FastGFile("C:\\Final\\classicaldance\\classicaldance\\logs\\output_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+        # Feed the image_data as input to the graph and get first prediction
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+        predictions = sess.run(softmax_tensor, \
+                               {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            # print('%s (score = %.5f)' % (human_string, score))
+            break
+
+
+
+    db=Db()
+    qry="select * from dance where dname='"+human_string+"'"
+    # print(qry)
+    res=db.selectOne(qry)
+    if res is  None:
+        return render_template('user/upload.html',status="no", res="Other")
+    else:
+        return render_template("user/upload.html",status='ok',res="A",dname=res['dname'],dimage=res['dimage'],ddescrip=res['ddescrip'])
+
+
+
+@app.route("/uploadpost",methods=['post'])
+def uploadpost():
+    import cv2
+    files= request.files["fileField"]
+    files.save("C:\\Final\\classicaldance\\classicaldance\\static\\2.jpg")
+
+    import numpy as np
+    # import pandas as pd
+    # import matplotlib.pyplot as plt
+    # import seaborn as sns
+    # import tensorflow as tf
+    # from tensorflow import keras
+    # import os
+    # import random
+    # from shutil import copyfile
+    #
+    # from tensorflow.keras import layers, models, optimizers, callbacks, Sequential
+    #
+    # from tensorflow.keras.layers import Flatten, Dense, BatchNormalization, Activation, Dropout, Conv2D, \
+    #     GlobalAveragePooling2D
+    from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input, InceptionResNetV2
+    #
+    # import cv2
+    # # from tensorflow_core.python.keras.models import load_model
+    #
+    # lrs = [0.0001, 0.00001]
+    # batch_sizes = [40, 50, 64]
+    # freeze_layers = [10, 20, 30, 40, 50]
+
+    # lr = lrs[0]
+    #
+    # base_model = InceptionResNetV2(include_top=False, weights='imagenet',
+    #                                pooling='avg', input_shape=(256, 256, 3))
+    # #
+    # model = Sequential()
+    # model.add(base_model)
+    # model.add(Flatten())
+    # model.add(Dense(1024, activation=('relu'), input_dim=512))
+    # model.add(Dense(512, activation=('relu')))
+    # model.add(Dropout(.3))
+    # model.add(Dense(256, activation=('relu')))
+    # model.add(Dropout(.3))
+    # model.add(Dense(128, activation=('relu')))
+    # model.add(Dropout(.2))
+    # model.add(Dense(8, activation=('softmax')))
+    #
+    # model.compile(loss="categorical_crossentropy",
+    #               optimizer=optimizers.Adam(lr=lr),
+    #               metrics=['accuracy'])
+
+    # from  tensorflow.keras.models import  load_model
+    model = load_model('C:\\Final\\classicaldance\\classicaldance\\a.h5')
+
+    image = cv2.resize(cv2.imread("C:\\Final\\classicaldance\\classicaldance\\static\\2.jpg"),(256, 256))
+    image = preprocess_input(image)
+    k = []
+    image = np.expand_dims(image, axis=0)
+    k.append(image)
+    import numpy as np
+    aaa = model.predict(image)
+
+    danceform = ["bharatanatyam", "kathak", "kathakali", "kuchipudi", "manipuri", "mohiniyattam", "odissi", "sattriya"]
+    a = np.argmax(aaa)
+
+    # print("final result", danceform[a])
+
+    return render_template('user/upload.html',res= danceform[a])
+
+@app.route('/forget')
+def forget():
+    return render_template('forgetpass.html')
+
+@app.route('/forgetpass_post',methods=['post'])
+def forgetpass_post():
+
+
+    emailid = request.form['textfield']
+    db=Db()
+    qry="select * from login where uname='"+emailid+"'"
+    r=db.select(qry)
+    if r is not None:
+
+        import  random
+
+        s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+        s.starttls()
+        s.login("aiswaryaharidas5428@gmail.com", "Aiswarya@5428")
+        msg = MIMEMultipart()  # create a message.........."
+        message = "Messege from password change request"
+        msg['From'] = "aiswaryaharidas5428@gmail.com"
+        msg['To'] = emailid
+        msg['Subject'] = "Your Password for classicaldance"
+
+        k= str(random.randint(10000,1000000))
+        body = "Your new password is : " +k
+        msg.attach(MIMEText(body, 'plain'))
+        s.send_message(msg)
+
+        db=Db()
+        db.update("update login set password='"+k+"' where uname='"+emailid+"' ")
+        return "<script>alert('Password changed successfully');window.location='/'</script>"
+    else:
+        return "<script>alert('Invalid email');window.location='/'</script>"
+
+
+
+
+
+
+@app.route("/temp")
+def temp():
+    return render_template("tables.html")
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,threaded=False)
